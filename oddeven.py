8cbc6206eb3a774766d983edb5e872764cfa4fd8
Almajoo <72122115+Almajoo@users.noreply.github.com>
2021-07-02 15:17:02 +0530

    program completed

--- oddeven.py
+++ oddeven.py
@@ -1,3 +1,8 @@
 term=int(input("Enter a number: "))
 try:
-    if term%2=0
+    if term%2==0:
+        print("Number is even")
+    else:
+        print("Number is odd")
+except:
+    print("Number is neither odd nor even")
